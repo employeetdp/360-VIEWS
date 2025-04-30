33db3d8c844189bae7ed51b97c7702d4e5203f74
employeetdp <employeetdp@gmail.com>
2025-04-30 10:29:31 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,634 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-tohpentrance-lobby-1",
+      "name": "TOHPENTRANCE LOBBY 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.0003593776000183624,
+        "pitch": 0,
+        "fov": 1.3926760049349705
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.2130757995440451,
+          "pitch": 0.06710011373321301,
+          "rotation": 5.497787143782138,
+          "target": "1-tohplobby-to-boulaverd-"
+        },
+        {
+          "yaw": -0.018984545664896046,
+          "pitch": -0.013318707741293068,
+          "rotation": 0,
+          "target": "2-tohpscene-8"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-tohplobby-to-boulaverd-",
+      "name": "TOHPLOBBY TO BOULAVERD ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.019174398166951434,
+        "pitch": -0.005612200329151662,
+        "fov": 1.2681200091521299
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7730426184736032,
+          "pitch": 0.014596883482042244,
+          "rotation": 0,
+          "target": "2-tohpscene-8"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-tohpscene-8",
+      "name": "TOHPScene 8",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 1.58053583468962,
+        "pitch": 0.03005606588990517,
+        "fov": 1.3926760049349705
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2524360553598761,
+          "pitch": 0.062498911903700716,
+          "rotation": 0,
+          "target": "3-tohpcoffee-bar-view-1"
+        },
+        {
+          "yaw": 0.6559180122547552,
+          "pitch": 0.04621437371835313,
+          "rotation": 4.71238898038469,
+          "target": "4-tohporchid-lift-lobby-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-tohpcoffee-bar-view-1",
+      "name": "TOHPCOFFEE BAR VIEW 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3926760049349705
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.020622670255433,
+          "pitch": 0.04340760030575197,
+          "rotation": 0.7853981633974483,
+          "target": "5-tohpcoffee-bar-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-tohporchid-lift-lobby-",
+      "name": "TOHPORCHID LIFT LOBBY ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7371435971742315,
+          "pitch": -0.009420505978727789,
+          "rotation": 1.5707963267948966,
+          "target": "3-tohpcoffee-bar-view-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-tohpcoffee-bar-1",
+      "name": "TOHPCOFFEE BAR 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.444438990790033,
+          "pitch": 0.0239764396746196,
+          "rotation": 0,
+          "target": "6-tohpscene-7"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-tohpscene-7",
+      "name": "TOHPScene 7",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.0006321239000115497,
+        "pitch": 0,
+        "fov": 1.3926760049349705
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "7-tohpfrom-atrium-passage-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-tohpfrom-atrium-passage-",
+      "name": "TOHPFROM ATRIUM PASSAGE ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.004027682889219619,
+        "pitch": -0.031441556552316285,
+        "fov": 1.3926760049349705
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5024289915984355,
+          "pitch": -0.02031431054432531,
+          "rotation": 0,
+          "target": "7-tohpfrom-atrium-passage-"
+        },
+        {
+          "yaw": -1.7549363096976816,
+          "pitch": 0.018050132271048014,
+          "rotation": 4.71238898038469,
+          "target": "8-tohpatrium-entry-_2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-tohpatrium-entry-_2",
+      "name": "TOHPATRIUM ENTRY _2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.564235072727417,
+          "pitch": 0.18366843924801657,
+          "rotation": 4.71238898038469,
+          "target": "9-tohpspiral-staircase-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-tohpspiral-staircase-",
+      "name": "TOHPSPIRAL STAIRCASE ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5135485526639307,
+          "pitch": 0.06181355826905488,
+          "rotation": 1.5707963267948966,
+          "target": "10-tohpscene-9"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-tohpscene-9",
+      "name": "TOHPScene 9",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.06993070663860301,
+          "pitch": 0.039245903861736764,
+          "rotation": 4.71238898038469,
+          "target": "11-tohpdining-from-atrium-passage_1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-tohpdining-from-atrium-passage_1",
+      "name": "TOHPDINING FROM ATRIUM PASSAGE_1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "rotation": 4.71238898038469,
+          "target": "12-tohpatrium-passage-_1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-tohpatrium-passage-_1",
+      "name": "TOHPATRIUM PASSAGE _1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7149516757295888,
+          "pitch": 0.06706778818815273,
+          "rotation": 4.71238898038469,
+          "target": "13-tohpatrium-buffet_1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-tohpatrium-buffet_1",
+      "name": "TOHPATRIUM BUFFET_1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "rotation": 1.5707963267948966,
+          "target": "14-tohpdinning-view-_1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-tohpdinning-view-_1",
+      "name": "TOHPDINNING VIEW _1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.052562358999804104,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "13-tohpatrium-buffet_1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "15-tohpr-floor-1",
+      "name": "TOHPR FLOOR 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.3960554841064088,
+        "pitch": 0.02219403991928459,
+        "fov": 1.3926760049349705
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3960554841064088,
+          "pitch": 0.02219403991928459,
+          "rotation": 0,
+          "target": "16-tohpr-floor-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "16-tohpr-floor-2",
+      "name": "TOHPR FLOOR 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.026967815229101078,
+        "pitch": 0.0050704961473080346,
+        "fov": 1.2681200091521299
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0508562058193824,
+          "pitch": -0.024006140758325856,
+          "rotation": 1.5707963267948966,
+          "target": "17-tohpr-floor-5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "17-tohpr-floor-5",
+      "name": "TOHPR FLOOR 5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
